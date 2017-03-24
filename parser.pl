a2adaf12cebd805ffd0b18f0fa187519c3160434
EnderRL <juanmiguel1996@gmail.com>
2017-03-24 15:20:54 +0100

    Adding problem

--- parser.pl
+++ parser.pl
@@ -10,14 +10,14 @@ instrucciones(P):-
     instrucciones(L2).
 
 instruccion(P):-
-    append(L1,['='|L2],P),
-    append(L3,['+'|L4],L2),
+    append(L1,[=|L2],P),
+    append(L3,[+|L4],L2),
     variable(L1),
     variable(L3),
     variable(L4).
 instruccion(P):-
     append([if|[]],L1,P),
-    append(L2,['='|L3],L1),
+    append(L2,[=|L3],L1),
     append(L4,[then|L5],L3),
     append(L6,[else|L7],L5),
     append(L8,[endif|[]],L7),
